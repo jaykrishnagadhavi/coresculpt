73401b4b76efa1e1500f86f3096d1aadf921e56f
jaykrishnagadhavi <jaykrishnagadhavi4@gmail.com>
2026-09-03 15:07:55 +0530

    feat: add NoGymSection component to highlight home-based workout benefits

--- src/components/NoGymSection.jsx
+++ src/components/NoGymSection.jsx
@@ -12,7 +12,7 @@ const NoGymSection = () => {
               <img 
                 src="/images/Product-Section-Img-1.jpg" 
                 alt="Home Workout" 
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover object-top"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent"></div>
               <div className="absolute bottom-6 left-6 right-6">
